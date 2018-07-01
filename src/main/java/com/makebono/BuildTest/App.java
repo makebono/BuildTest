c531fa403ad9b484c88e597489c97579d6d77e1a
makebono <xujiant@sfu.ca>
2018-07-01 09:29:57 -0700

    Test

---
+++ src/main/java/com/makebono/BuildTest/App.java
@@ -0,0 +1,13 @@
+package com.makebono.BuildTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
